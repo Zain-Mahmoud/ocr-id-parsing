cbce6b6212472bc67c53713e7bee1a9f146d65ff
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-07-19 16:07:20 +0300

    updated fine-tuning and inference

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 from unsloth import FastVisionModel
 import json 
+from PIL import Image, ImageEnhance
 
 field_structure = {
     "full_name": "string (arabic)",
@@ -14,11 +15,15 @@ USER_PROMPT = f'''
 '''
 
 
-model, tokenzier = FastVisionModel.from_pretrained("./models/model_name", load_in_4bit=False)
+def preprocess_image(image: Image):
+    grey_image = image.convert('L')
+    enhancer = ImageEnhance.Contrast(grey_image)
+    enhanced_image = enhancer.enhance(1.5)
+
+    return enhanced_image
 
 
-def infer(model, tokenizer, sample):
-    
+def infer(model, tokenizer, sample: Image):
     FastVisionModel.for_infer(model)
 
     messages = [
@@ -35,9 +40,9 @@ def infer(model, tokenizer, sample):
             ]
         }
     ]
-
+    preprocessed_image = preprocess_image(sample)
     input_text = tokenizer.apply_chat_templates(messages, add_generation_prompt=True)
-    inputs = tokenizer(sample, input_text, add_special_tokens=False, return_tensors='pt').to(model.device)
+    inputs = tokenizer(preprocessed_image, input_text, add_special_tokens=False, return_tensors='pt').to(model.device)
 
     inference = model.generate(**inputs, max_new_tokens=128, use_cache=True, temperature=1.5, min_p=0.1)
 
@@ -47,10 +52,12 @@ def infer(model, tokenizer, sample):
 def batch_infer(model, tokenizer, samples):
     FastVisionModel.for_infer(model)
     predictions = []
+
     for sample in samples:
-        try:
-            prediction = json.loads(infer(model, tokenizer, sample['image']))
-        except json.JSONDecodeError:
-            prediction = None
+        prediction = infer(model, tokenizer, sample)
         predictions.append(prediction)
+
     return predictions
+
+
+model, tokenzier = FastVisionModel.from_pretrained("./models/model_name", load_in_4bit=False)
