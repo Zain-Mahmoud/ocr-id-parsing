d5ea66830e7115706dd71bd5d43383bc1fee892e
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-08-13 12:59:31 +0300

    add post endpoint

--- src/main.py
+++ src/main.py
@@ -7,14 +7,14 @@ yolo = models["yolo"]
 vlm_generation, vlm_tokenizer = models["vlm"]
 ocr_model = models["ocr"]
 
-# app = FastAPI()
+app = FastAPI()
 
-# @app.post("/predict_image/")
-# async def predict_image(file: UploadFile):
-#     try:
-#         im = Image.open(file.file)
-#     except:
-#         return {"Error": 404}
-#     response = predict(yolo, vlm_generation, vlm_tokenizer, ocr_model, im)
-#     return response
+@app.post("/predict_image/")
+async def predict_image(file: UploadFile):
+    try:
+        im = Image.open(file.file)
+    except:
+        return {"Error": 404}
+    response = predict(yolo, vlm_generation, vlm_tokenizer, ocr_model, im)
+    return response
 
