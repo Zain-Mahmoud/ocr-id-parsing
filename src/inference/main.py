33bd1462168c0813e8637d19de2a4de2d1a34187
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-08-16 20:46:17 +0300

    update endpoint

--- src/inference/main.py
+++ src/inference/main.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
-from .infer import predict, load
+from infer import predict, load
 from PIL import Image, ImageOps
 import io 
 
 models = load()
 yolo = models["yolo"]
 vlm_generation, vlm_tokenizer = models["vlm"]
-ocr_model = models["ocr"]
+digit_model = models["digit_model"]
+ocr_reader = models["ocr_reader"]
 
 app = FastAPI()
 
-@app.post("/predict_image/")
-async def predict_image(file: UploadFile):
+@app.post("/get_info/")
+async def get_info(file: UploadFile):
     raw_img = await file.read()
     im = Image.open(io.BytesIO(raw_img))
     im = ImageOps.exif_transpose(im)
-    response = predict(yolo, vlm_generation, vlm_tokenizer, ocr_model, im)
+    response = predict(yolo, vlm_generation, vlm_tokenizer, digit_model, ocr_reader, im)
     return response
-
